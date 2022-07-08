b7aacf2ed5193805079a91464940edf606fb024e
Kamil Gierada <vlaska8888@gmail.com>
2022-07-08 22:56:47 +0200

    Fix path to package finding

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import setup, find_packages
 setup(
     name='robomania',
     version='0.0.1',
-    packages=find_packages(where='robomania'),
+    packages=find_packages(where='src'),
     package_dir={'': 'src'},
     zip_safe=True,
 )
