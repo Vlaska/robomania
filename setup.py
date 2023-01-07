2c752e568e08bb7a9075b187334ade867dd6b130
Kamil Gierada <vlaska8888@gmail.com>
2023-01-07 07:47:49 +0100

    Version bump

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import find_packages, setup
 
 setup(
     name='robomania',
-    version='0.3.1',
+    version='0.4.0',
     packages=find_packages(where='src'),
     package_dir={'': 'src'},
     zip_safe=True,
